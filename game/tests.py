c4be19000de31a0a1465ef56fc82e4a8e0b9840a
吉澤達也 <yoshizawatatsuya@yoshizawatatsuya-no-MacBook-Pro.local>
2018-11-11 18:41:26 +0900

    add:テストコードのサンプルを作成

--- game/tests.py
+++ game/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+
+import logging
+
+from game.models import Word
+
+
+class WordTests(TestCase):
+
+    def test_get(self):
+        logger = logging.getLogger(__name__)
+
+        data = {
+            'name': 'テスト単語1',
+        }
+
+        Word.objects.create(**data)
+
+        url = reverse('word-list')
+        logger.info(url)
+        response = self.client.get(url)
+        logger.info(response.json())
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), [{
+            'id': 1,
+            'name': data['name'],
+        }])
