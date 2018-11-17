151dcf5a23641e21dd1fb5cf9153b141a54bf6b4
吉澤達也 <yoshizawatatsuya@yoshizawatatsuya-no-MacBook-Pro.local>
2018-11-17 18:53:31 +0900

    消し忘れコードの削除

--- game/models.py
+++ game/models.py
@@ -26,24 +26,6 @@ class Game(models.Model):
     is_deleted = models.BooleanField(default=False)
 
 
-class Game(models.Model):
-    SINGLE_MODE = 'single'
-    MULTI_MODE = 'multi'
-    MODE_CHOICES = (
-        (SINGLE_MODE, '1人用'),
-        (MULTI_MODE, '複数対戦')
-    )
-    member_count = models.IntegerField()
-    mode = models.CharField(
-        max_length=255,
-        choices=MODE_CHOICES,
-        default=SINGLE_MODE
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_deleted = models.BooleanField(default=False)
-
-
 class GameDetail(models.Model):
     game = models.ForeignKey(Game, on_delete=models.PROTECT)
     member_name = models.CharField(max_length=255)
